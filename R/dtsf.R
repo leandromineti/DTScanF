cfb3a13892fbaee74feca8be06b79621a049a0ac
leandro <leandromineti@gmail.com>
2017-11-07 20:38:51 -0200

    Include regularization option in dtsf().

--- R/dtsf.R
+++ R/dtsf.R
@@ -9,6 +9,8 @@
 #' @param best an integer defining how many matches will be used in the forecasting.
 #' @param window an integer indicating the size of the window to match.
 #' @param forecast number of data points to be forecasted.
+#' @param reg logical indicating if series regularization should be performed. 
+#' Default = FALSE.
 #'
 #' @return a \code{list} with two objects:
 #' \itemize{
@@ -22,10 +24,19 @@
 #' @examples
 #'
 #' @family gbdcd
-dtsf <- function(ts, poli, best, window, forecast) {
+dtsf <- function(ts, poli, best, window, forecast, reg = FALSE) {
   
-  # Data wrangling
-  wind <- cbind(1, ts)
+  ts_reg <- ts  # If reg = FALSE, ts_reg remains equal to ts
+  
+  # Regularization
+  if(reg) {
+    for(i in 2:(length(ts)-1)) {
+      ts_reg[i] <- 0.99*dt$vento[i] + 0.005*dt$vento[i-1] + 0.005*dt$vento[i+1]
+    }
+  }
+  
+  # Handling polynomial order
+  wind <- cbind(1, ts_reg)
   while(dim(wind)[2]-1!=poli) wind <- cbind(wind, train^dim(wind)[2])
   
   # Run window through data
@@ -39,7 +50,7 @@ dtsf <- function(ts, poli, best, window, forecast) {
   wind_forecast <- c()
   for(best_index in 1:best){ # Run it until the highest best match
     pos_train <- sequence$start[position[best_index]] : sequence$stop[position[best_index]]
-    model <- lm(y ~ poly(x, poli, raw = T), data=data.frame(x = ts[pos_train]))
+    model <- lm(y ~ poly(x, poli, raw = T), data=data.frame(x = ts_reg[pos_train]))
     pos_test <- (sequence$stop[position[best_index]]+1):(sequence$stop[position[best_index]]+forecast)
     wind_forecast <- rbind(wind_forecast, predict(model, newdata=data.frame(x = ts[pos_test])))
   }
